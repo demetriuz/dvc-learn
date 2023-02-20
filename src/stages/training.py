94d70a0b97264b0bd1c6f5b4a5f50628cae20006
Dmitrii Lazarko <alt0064@gmail.com>
2023-02-20 12:19:54 +0400

    upd

--- src/stages/training.py
+++ src/stages/training.py
@@ -1,12 +1,13 @@
 from sklearn.linear_model import LogisticRegression
 from src.utils import read_yaml
 import joblib
+from typing import Dict
 
 
-def training(dataset_path: str, model_path: str):
+def training(dataset_path: str, model_path: str, model_kw: Dict):
     iris = joblib.load(dataset_path)
 
-    log_reg = LogisticRegression(max_iter=7)
+    log_reg = LogisticRegression(**model_kw)
     log_reg.fit(iris["X_train"], iris["y_train"])
 
     joblib.dump(log_reg, model_path)
@@ -15,5 +16,6 @@ def training(dataset_path: str, model_path: str):
 if __name__ == "__main__":
     dataset_path = read_yaml("params.yaml")["training"]["dataset_path"]
     model_path = read_yaml("params.yaml")["training"]["model_path"]
+    model_kw = read_yaml("params.yaml")["training"]["model_kw"]
 
-    training(dataset_path, model_path)
+    training(dataset_path, model_path, model_kw)
